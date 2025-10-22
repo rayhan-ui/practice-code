89af120c0bfaea349d816316f5b5f1677d6a284f
Rayhan <webdeveloperrayhan@gmail.com>
2025-10-23 00:40:58 +0600

    part-two-useref

--- src/App.js
+++ src/App.js
@@ -8,15 +8,20 @@ import Jsx from "./partOne/Jsx";
 import ListNKeys from "./partOne/ListNKeys.jsx";
 import State from "./partOne/State.jsx";
 import Count from "./partTwo/Count.jsx";
+import Live from "./partTwo/UseRef.jsx";
 import UseEffect from "./partTwo/UseEffect.jsx";
+import UseRef from "./partTwo/UseRef.jsx";
 
 export default function App(){
   return(
     <>
+    <div style={{ textAlign: "center", marginBottom: "40px", justifyContent: "center", }}>
       {/* <ConditionalRendering /> */}
       {/* <ListNKeys /> */}
       {/* <Count/> */}
-      <UseEffect />
+      {/* <UseEffect /> */}
+      <UseRef />
+    </div>
 
     </>
   )  
---
+++ src/partTwo/UseRef.jsx
@@ -0,0 +1,19 @@
+import { useRef } from "react"
+
+export default function UseRef(){
+  const inputRef = useRef(null)
+
+  function handleClick(){
+    inputRef.current.focus()
+  }
+  return(
+    <>
+      <input 
+        ref={inputRef} 
+        style={{border: "2px solid blue", height: "40px", borderRadius: "4px", padding: "8px"}} 
+        type="text" 
+        placeholder="write something..." />
+      <button onClick={handleClick}>click</button>
+    </>
+  )
+}
